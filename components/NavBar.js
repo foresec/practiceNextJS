5e2f55b6ba50dc994a2aaed2a04a51b22af6b4ed
foresec <foresec@naver.com>
2024-01-19 09:18:38 +0900

    0119-3

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,19 +1,24 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css"
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
-    <nav className={styles.nav}>
+    <nav>
       <Link
-        style={{ color: router.pathname === "/" ? "black" : "lightgray" }}
+        className={`${styles.link} ${
+          router.pathname === "/" ? styles.active : "lightgray"
+        }`}
         href="/"
       >
         Home
       </Link>
       <Link
-        style={{ color: router.pathname === "/about" ? "black" : "lightgray" }}
+        className={[
+          styles.link,
+          router.pathname === "/about" ? styles.active : "lightgray",
+        ].join(" ")}
         href="/about"
       >
         About
